a6118704d072877dfc70c0045643a2b049dc9b36
chaitanya <chaitanya.kadian@jungleworks.co>
2020-06-12 12:02:53 -0600

    initial commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pggUser: process.env.PGGUSER,
+    pggHost: process.env.PGGHOST,
+    pggDatabase: process.env.PGGDATABASE,
+    pggPassword: process.env.PGGPASSWORD,
+    pggPort: process.env.PGGPORT
+};
